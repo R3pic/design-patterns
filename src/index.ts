cbd2bdbeda0ea7b462624b4857601a00f30b3141
R3pic <101osc@naver.com>
2025-03-30 17:02:47 +0900

    chore: CLI 디자인 수정

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import { DesignPatternCli } from '@utils/design-pattern-cli-interactor.ts';
 async function main() {
   const loader = new DesignPatternLoader();
   const cli = new DesignPatternCli(await loader.load());
+  while (1) {
+    try {
+      console.info('========================================');
+      const runner = await cli.select();
 
-  const runner = await cli.select();
-
-  console.info(`${runner.constructor.name}을 실행중....`);
-  console.info('========================================');
-  runner.run();
+      console.info(`${runner.constructor.name}을 실행중....`);
+      console.info('========================================');
+      runner.run();
+    } catch (e) {
+     console.error(e);
+    }
+  }
 }
 
 main().catch(console.error);
